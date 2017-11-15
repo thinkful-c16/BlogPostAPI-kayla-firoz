0ea82783bb3d6b8e90072d51bc373368354ef611
Kayla Webb <kaylarwebb@gmail.com>
2017-11-15 16:15:09 -0500

    app post working.

--- server.js
+++ server.js
@@ -8,12 +8,26 @@ const {BlogPosts} = require('./model');
 const jsonParser = bodyParser.json();
 const app = express();
 
-// BlogPosts.create('something', 'blah blah', 'kayla');
+BlogPosts.create('something', 'blah blah', 'kayla');
+BlogPosts.create('hello word', 'something about the universe', 'firoz');
+
 
 app.get('/blog-posts', function(req, res) {
   res.json(BlogPosts.get());
 });
 
+app.post('/blog-posts', jsonParser, function(req, res){
+  const fields = ['title', 'content', 'author'];
+  for (let i=0; i < fields.length; i++) {
+    if (!(fields[i] in req.body)) {
+      console.log(fields);
+      return `Status ${res.status(400)} missing ${fields[i]}`;
+    }
+  }
+  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  return res.status(201).json(post);
+});
+
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
