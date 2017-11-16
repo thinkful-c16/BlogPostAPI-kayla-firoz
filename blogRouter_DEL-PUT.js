4a0f609c6604009be859a217605b42a3f0754f4c
Firoz Kamdar <firozkamdar@gmail.com>
2017-11-15 19:24:03 -0800

    router set up and functioning for delete and put

---
+++ blogRouter_DEL-PUT.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./model.js');
+
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const fields = ['title', 'content', 'author', 'id'];
+  for (let i = 0; i < fields.length; i++){
+    if (!(fields[i] in req.body )) {
+      return `Error ${res.status(400)} missing ${fields[i]}`;
+    } else if (req.params.id !== req.body.id) {
+      return `Error ${res.status(400)} ${req.params.id} must match ${req.body.id}`;
+    } 
+  }
+    
+  BlogPosts.update({id: req.params.id, title: req.body.title, content: req.body.content, author: req.body.author});  
+  console.log(req.params); 
+  res.status(204).end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,43 +14,7 @@ const blogRouter = require('./blogRouter_GET-POST');
 const blogRouterDelPut = require('./blogRouter_DEL-PUT');
 
 app.use('/blog-posts', blogRouter);
-app.use('/blog-posts/:id', blogRouter);
-
-
-//app.use('/blog-posts', blogRouter);
-
-// app.post('/blog-posts', jsonParser, function(req, res){
-//   const fields = ['title', 'content', 'author'];
-//   for (let i=0; i < fields.length; i++) {
-//     if (!(fields[i] in req.body)) {
-//       return `Status ${res.status(400)} missing ${fields[i]}`;
-//     }
-//   }
-//   const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
-//   return res.status(201).json(post);
-// });
-
-app.delete('/blog-posts/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  res.status(204).end();
-});
-
-app.put('/blog-posts/:id', jsonParser, (req, res) => {
-  const fields = ['title', 'content', 'author', 'id'];
-  for (let i = 0; i < fields.length; i++){
-    if (!(fields[i] in req.body )) {
-    
-      return `Error ${res.status(400)} missing ${fields[i]}`;
-    } else if (req.params.id !== req.body.id) {
-      return `Error ${res.status(400)} ${req.params.id} must match ${req.body.id}`;
-    } 
- 
-  }
-  
-  BlogPosts.update({id: req.params.id, title: req.body.title, content: req.body.content, author: req.body.author});  
-  console.log(req.params); 
-  res.status(204).end();
-});
+app.use('/blog-posts', blogRouterDelPut);
 
 
 app.listen(process.env.PORT || 8080, () => {
